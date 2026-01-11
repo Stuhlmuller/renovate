306366b62f62a324ca2355849f5e8bf01191b8e4
Rodman <rodman@stuhlmuller.net>
2026-01-11 15:22:29 -0800

    refactor: update Renovate configuration files
    - Simplified `renovate.json` by removing unnecessary extends and package rules.
    - Enhanced `.github/renovate-global.js` with additional configuration options for automerging and semantic commits.
    - Adjusted cron schedule in `.github/workflows/renovate.yml` to run daily instead of every 15 minutes.

--- .github/renovate-global.js
+++ .github/renovate-global.js
@@ -1,9 +1,21 @@
 module.exports = {
-    platform: 'github',
-    globalExtends: [
-        'config:best-practices'
-    ],
-    repositories: [],
-    platformCommit: 'enabled',
-    autodiscover: true
-}
+	platform: "github",
+	globalExtends: [
+		"config:best-practices",
+		":semanticCommits",
+		":automergeMinor",
+		":automergeBranch",
+		"schedule:automergeWeekly",
+		":rebaseStalePrs"
+	],
+	repositories: [],
+	platformCommit: "enabled",
+	autodiscover: true,
+	packageRules: [
+		{
+			matchUpdateTypes: ["minor", "patch"],
+			matchCurrentVersion: "!/^0/",
+			automerge: true
+		}
+	]
+};
